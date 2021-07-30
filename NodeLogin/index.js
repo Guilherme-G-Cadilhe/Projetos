9d5757f2c8b78dc465aeec4b1764d32b47ccae79
Bobnini <guilhermegcadilhe@gmail.com>
2021-07-30 14:15:15 -0300

    NodeLogin Project
    Testing a log in mockup

---
+++ NodeLogin/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const session = require("express-session"); // Autenticação de sessão
+
+// Basicos
+const port = process.env.PORT || 3000;
+let path = require("path");
+const app = express();
+
+// Configurando a autenticação de sessão de log in
+app.use(session({ secret: "teste" })); //Define uma chave para a sessão
+
+// Habilitando HTMLS, CSS, etc
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "/views"));
+
+//Habilitando parsing das informações POST vindas no 'req.body' e json
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Dados teste
+const login = "admin";
+const password = "123";
+
+// Rotas de acesso na página
+app.post("/", (req, res) => {
+  if (req.body.username == login && req.body.password == password) {
+    // Testa se é a conta
+    // Logado
+    req.session.username = login;
+    res.render("logado", { username: login });
+  } else {
+    res.render("index");
+  }
+  const data = req.body;
+  console.log(data);
+});
+app.get("/", (req, res) => {
+  if (req.session.username) {
+    res.render("logado", { username: login });
+    console.log(` O meu usuario logado é ${req.session.username}`);
+  } else {
+    res.render("index");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Escutando na porta ${port} em localhost:${port}`);
+});
