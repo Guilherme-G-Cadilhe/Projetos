4f2a22e1513bf5c4c170e2cd323b2877781ed110
Bobnini <guilhermegcadilhe@gmail.com>
2021-08-30 17:58:43 -0300

    Validação de formulario com classe

---
+++ _Cursos/Javascript e Typescript do Básico ao Avançado/Seção 06 - Classes/86Aula-Formulario/assets/js/ClasseValidandoCPF.js
@@ -0,0 +1,49 @@
+class ValidaCPF {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, "cpfLimpo", {
+      writable: false,
+      enumerable: true,
+      configurable: false,
+      value: cpfEnviado.replace(/\D+/g, ""),
+    });
+  }
+
+  eSequencia() {
+    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  }
+
+  static geraDigito(cpfSemDigitos) {
+    let total = 0;
+    let reverso = cpfSemDigitos.length + 1;
+
+    for (let stringNumerica of cpfSemDigitos) {
+      total += reverso * Number(stringNumerica);
+      reverso--;
+    }
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : "0";
+  }
+
+  geraNovoCpf() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    this.novoCPF = cpfSemDigitos + digito1 + digito2;
+  }
+
+  valida() {
+    if (!this.cpfLimpo) return false;
+    if (typeof this.cpfLimpo !== "string") return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.eSequencia()) return false;
+    this.geraNovoCpf();
+
+    return this.novoCPF === this.cpfLimpo;
+  }
+}
+
+// const validacpf = new ValidaCPF("177.027.237-26");
+
+// if (validacpf.valida()) {
+//   console.log("CPF Válido");
+// }
