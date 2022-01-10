ed2e6fc63e3c0b57bb0e6f191c8f8c3a1929ecff
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2022-01-10 17:59:25 -0300

    Initial Commit

---
+++ React HookForm Challenge/react-hook-form-challenge/src/App.js
@@ -0,0 +1,18 @@
+// dropzone to drag and drop files
+// yup to validate the fields
+// libhponenumber-js to validate phoe numbers
+// hookform/resolvers to be able to use yup with react hook form
+// react confetti
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
