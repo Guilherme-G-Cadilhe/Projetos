6e9140292b3fe9b18a9980f9fc139c193316d003
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2022-03-08 17:33:36 -0300

    Start of Dave's Form tutorial

---
+++ _Cursos/React e Node/Dave_Gray_Tutorials/Complete_Login_Feature/react_register_form-main/src/Register.js
@@ -0,0 +1,31 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/; //Maisculo, minusculo, numeros, e '-' e '_'
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/; // Conter letra maiscula, minuscula, numeros e caracteres especiaisimport React from 'react'
+
+
+const Register = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+
+    return (
+        <div>Register</div>
+    )
+}
+
+export default Register
