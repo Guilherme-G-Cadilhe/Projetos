f253f4527707a01c5dcff0af054caf5baaa35b80
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2022-03-16 08:47:07 -0300

    update register.js

--- _Cursos/React e Node/Dave_Gray_Tutorials/Complete_Login_Feature/react_register_form-main/src/Register.js
+++ _Cursos/React e Node/Dave_Gray_Tutorials/Complete_Login_Feature/react_register_form-main/src/Register.js
@@ -22,9 +22,63 @@ const Register = () => {
     const [validMatch, setValidMatch] = useState(false);
     const [matchFocus, setMatchFocus] = useState(false);
 
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log('result user', result);
+        console.log('user', user);
+        setValidName(result)
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log('result password', result);
+        console.log('pwd', pwd);
+        setValidPwd(result)
+        const match = pwd === matchPwd;
+        setValidMatch(match)
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd, matchPwd])
 
     return (
-        <div>Register</div>
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Registre-se</h1>
+            <form>
+                <label htmlFor="username">
+                    Usuário:
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    required
+                    aria-invalid={validName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 a 24 Caractéres.<br />
+                    Precisa começar com uma letra.<br />
+                    Letras, Números, Hífens, sublinhados, são permitidos.<br />
+                </p>
+                <label htmlFor=""></label>
+            </form>
+
+        </section>
     )
 }
 
