5aea70e37cede9f80b73e74b4786e288649a5e95
Guilherme Cadilhe <guilhermegcadilhe@gmail.com>
2023-05-12 10:53:52 -0300

    Redux Toolkit Learning

---
+++ _Cursos/CompletReactDeveloper2022/crwn-clothing-v2-redux-toolkit-main/src/store/user/user.reducer.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import { USER_ACTION_TYPES } from './user.types';
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  "initialState": INITIAL_STATE,
+  "reducers": {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload
+    }
+  }
+})
+
+export const { setCurrentUser } = userSlice.actions
+
+export const userReducer = userSlice.reducer;
+
+
+// export const userReducerOld = (state = INITIAL_STATE, action) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case USER_ACTION_TYPES.SET_CURRENT_USER:
+//       return { ...state, currentUser: payload };
+//     default:
+//       return state;
+//   }
+// };
